2adaea57aa791e9b2382c9ca79df5ac27deb8b28
neerajkhattar02 <nrjkhttr@gmail.com>
2026-02-11 20:23:31 +0530

    Update schemas.py

--- backend/schemas.py
+++ backend/schemas.py
@@ -5,6 +5,6 @@ class AQIRequest(BaseModel):
     no2: float = Field(ge=0, le=500)
     spm: float = Field(ge=0, le=1000)
     month: int = Field(ge=1, le=12)
-    age_group: str
-    has_respiratory_issue: bool
-    activity: str
+    age_group: str | None = None
+    has_respiratory_issue: bool | None = None
+    activity: str | None = None
